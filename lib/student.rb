962320372ec6413a63d243080edf52265b8c3875
Nataliya R <n.rudenko@gmail.com>
2019-04-30 15:53:09 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -61,7 +61,8 @@ class Student
      SELECT * FROM students WHERE grade = 10 ORDER BY ASC LIMIT 1 
      SQL
       DB[:conn].execute(sql).map do |row|
-        
+         self.new_from_db(row)
+       end.first
      end 
    
    
